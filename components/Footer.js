e46d207a652cdb7c065cba2a1ff9f753b5f64580
Fantasy Shao <forever.fantasy27@gmail.com>
2015-07-25 11:00:36 +0800

    Site components

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    render () {
+        return (
+            <footer className="site-footer bottom">
+                <div>
+                    <p>Copyright &copy; 2014-2015 Fantasy Shao.</p>
+                </div>
+            </footer>
+        )
+    }
+}
--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-/**
- * index.js
- *
- * Copyright 2015 Tuniu.com
- *
- * @description
- * @author shaochunhua@tuniu.com
- * @create 2015-07-25
- * @update 2015-07-25
- */
+import React from 'react';
+import Home from './components/Home';
+
+React.render(
+    <Home />,
+    document.getElementById('home')
+);
