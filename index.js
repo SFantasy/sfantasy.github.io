ad9cbb0480180a533c717894deef61a4d07cd4b2
Fantasy Shao <forever.fantasy27@gmail.com>
2015-07-27 23:19:45 +0800

    Add react-router, update index.html

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import Router, { Route, RouteHandler, DefaultRoute } from 'react-router';
 import Home from './components/Home';
+import Design from './components/Design/index';
+import About from './components/About/index';
 
-React.render((
-    <Home />
-), document.getElementById('home'));
+var App = React.createClass({
+    render () {
+        return (
+            <RouteHandler />
+        );
+    }
+});
+
+var routes = (
+    <Route handler={App} path="/">
+        <DefaultRoute name="home" handler={Home} />
+        <Route name="design" path="design" handler={Design} />
+        <Route name="about" path="about" handler={About} />
+    </Route>
+);
+
+
+Router.run(routes, Router.HistoryLocation, (Root) => {
+    React.render((
+        <Root />
+    ), document.getElementById('home'));
+});
