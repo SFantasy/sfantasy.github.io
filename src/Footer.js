f25aa59572147e48d667156f8537206b2dfe2b21
Fantasy <forever.fantasy27@gmail.com>
2022-03-29 11:11:37 +0800

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -8,9 +8,6 @@ export default class Footer extends Component {
           <p className="footer-section__title">Write</p>
           <ul className="footer-section__list">
             <li><a href="http://blog.fantasy.codes">Blog</a></li>
-            <li><a href="https://www.gitbook.com/@sfantasy">Gitbook</a></li>
-            <li><a href="http://www.jianshu.com/users/7YqELy/latest_articles">简书</a></li>
-            <li><a href="https://zhuanlan.zhihu.com/fantasy-land">知乎专栏</a></li>
           </ul>
         </div>
         <div className="footer-section">
@@ -18,9 +15,7 @@ export default class Footer extends Component {
           <ul className="footer-section__list">
             <li><a href="https://github.com/SFantasy">Github</a></li>
             <li><a href="http://weibo.com/shaofantasy">Weibo</a></li>
-            <li><a href="https://www.douban.com/people/fanTasy00/">Douban</a></li>
             <li><a href="https://unsplash.com/@fantasyshao/collections">Unsplash</a></li>
-            <li><a href="http://fantasyshao.lofter.com">Lofter</a></li>
           </ul>
         </div>
         <div className="footer-section">
